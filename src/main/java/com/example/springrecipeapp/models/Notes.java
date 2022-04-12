5b7170ab5f0c6b50d597876e0777ae149876d44c
abolfazl <A.kabiri@Tosan.com>
2022-04-12 14:51:45 +0430

    implement Recipe and Notes and their relation. closes #4

---
+++ src/main/java/com/example/springrecipeapp/models/Notes.java
@@ -0,0 +1,32 @@
+package com.example.springrecipeapp.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String notes;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
