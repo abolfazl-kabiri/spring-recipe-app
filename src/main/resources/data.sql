575c30477557cb57607471d9c75bb3366080e132
abolfazl <A.kabiri@Tosan.com>
2022-04-13 12:50:03 +0430

    add database initialization file. closes #10

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO categery (categoryName) VALUES ('American');
+INSERT INTO categery (categoryName) VALUES ('Italian');
+INSERT INTO categery (categoryName) VALUES ('Fast Food');
+INSERT INTO categery (categoryName) VALUES ('Persian');
+
+INSERT INTO unit_of_measure (description) VALUES ('tea spoon');
+INSERT INTO unit_of_measure (description) VALUES ('table spoon');
+INSERT INTO unit_of_measure (description) VALUES ('cup');
+INSERT INTO unit_of_measure (description) VALUES ('pinch');
