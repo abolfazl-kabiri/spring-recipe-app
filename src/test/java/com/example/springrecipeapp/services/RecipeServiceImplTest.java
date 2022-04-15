90cc58c5de56800ddb073da0876b130ddcb11822
abolfazl-kabiri <abolfazl.kb81@gmail.com>
2022-04-16 01:17:29 +0430

    add RecipeServiceImpl test. closes #22

---
+++ src/test/java/com/example/springrecipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,56 @@
+package com.example.springrecipeapp.services;
+
+import com.example.springrecipeapp.models.Recipe;
+import com.example.springrecipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class RecipeServiceImplTest {
+
+    @Mock
+    RecipeRepository repository;
+
+    @InjectMocks
+    RecipeServiceImpl service;
+
+    Set<Recipe> recipes;
+    Recipe recipe;
+
+    @BeforeEach
+    void setUp() {
+        recipes = new HashSet<>();
+        recipe = Recipe.builder().categories(new HashSet<>()).ingredients(new HashSet<>()).build();
+        recipes.add(recipe);
+    }
+
+    @Test
+    void delete() {
+
+        service.delete(recipe);
+        assertNull(service.findByDescription(recipe.getDescription()));
+    }
+
+
+    @Test
+    void getRecipes() {
+
+        when(repository.findAll()).thenReturn(recipes);
+        Set<Recipe> r = service.getRecipes();
+        assertEquals(1, r.size());
+        verify(repository).findAll();
+    }
+
+
+}
