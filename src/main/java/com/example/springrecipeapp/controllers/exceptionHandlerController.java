75752b153a3f17272b1614571155722471f7a85a
abolfazl-kabiri <abolfazl.kb81@gmail.com>
2022-04-24 02:06:58 +0430

    refactor exceptionHandler to controller advice.

---
+++ src/main/java/com/example/springrecipeapp/controllers/exceptionHandlerController.java
@@ -0,0 +1,18 @@
+package com.example.springrecipeapp.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class exceptionHandlerController {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public String handleNumberFormat(Exception exception, Model model) {
+        model.addAttribute("exception", exception);
+        return "400error";
+    }
+}
