af751bee5ca8b7e82970b31d8120f3275bbcbf8f
abolfazl-kabiri <abolfazl.kb81@gmail.com>
2022-04-24 01:52:30 +0430

    add display error data. closes #45

--- src/main/java/com/example/springrecipeapp/controllers/RecipeController.java
+++ src/main/java/com/example/springrecipeapp/controllers/RecipeController.java
@@ -65,7 +65,8 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public String handleNotFound() {
+    public String handleNotFound(Exception exception, Model model) {
+        model.addAttribute("exception", exception);
         return "404error";
     }
 
